643a9d3a8229070dc259fcbada495f48eb5d82e2
Austin Gil <austin@stegosource.com>
2018-12-01 08:22:03 -0800

    Creating graphql yoga server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+// TODO: Express middleware for cookies (JWT)
+// TODO: Express middleware for current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  deets => {
+    console.log(`Server is running on http://localhost${deets.port}`);
+  }
+);
